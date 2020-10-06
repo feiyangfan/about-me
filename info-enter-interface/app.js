557600594e3b24ed2322b67bcc980e049f3b38a5
Feiyang Fan <feiyangfan1@gmail.com>
2020-10-05 22:49:43 -0400

    add about me edit

--- info-enter-interface/app.js
+++ info-enter-interface/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const Project = require("./models/Project");
+const About = require("./models/Aboutme");
 
 require("dotenv").config();
 
@@ -21,16 +22,32 @@ mongoose.connect(`${process.env.MONGODB_URI}`, {
 // Routes
 app.get("/", async (req, res) => {
   const projects = await Project.find();
+  const about = await About.find();
   // console.log(projects);
-  res.render("index", { projects: projects });
+  res.render("index", { projects: projects, about: about });
 });
 
-app.post("/sendData", async (req, res) => {
+app.post("/sendAData", async (req, res) => {
+  // await console.log(req.body);
+  await About.create({
+    content: req.body.content,
+  });
+  res.redirect("/");
+});
+
+app.post("/deleteAbout", async (req, res) => {
+  // await console.log(req.body);
+  await About.deleteMany({});
+  res.redirect("/");
+});
+
+app.post("/sendPData", async (req, res) => {
   // await console.log(req.body);
   await Project.create({
-    name: req.body.name,
-    desc: req.body.desc,
-    details: req.body.detail,
+    name: req.body.pname,
+    desc: req.body.pdesc,
+    details: req.body.pdetail,
+    githubLink: req.body.githubLink,
   });
   res.redirect("/");
 });
@@ -40,6 +57,15 @@ app.post("/deleteProject", async (req, res) => {
   await Project.deleteOne({ _id: req.body.id });
   res.redirect("/");
 });
+
+app.post("/editProject", async (req, res) => {
+  // await console.log(req.body);
+  const project = await Project.findOne({ _id: req.body.id });
+  console.log(project);
+  res.render("edit", { project: project });
+});
+
+app.post("");
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
